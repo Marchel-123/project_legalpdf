151642225155076128e14460a6859462d4dd9253
chella83 <141978705+Marchel-123@users.noreply.github.com>
2024-10-07 14:11:02 +0800

    openai_requests.py
    Updated file

--- requests.py
+++ requests.py
@@ -1,21 +1,26 @@
-# import requests
 
-# api_key = ''
+import requests
+from dotenv import load_dotenv
+import os
 
-# data = {
-#     "model": "gpt-3.5-turbo",  
-#     "messages": [{"role": "user", "content": "Say this is a test!"}],
-#     "temperature": 0.7
-# }
+load_dotenv()
 
-# headers = {
-#     "Content-Type": "application/json",
-#     "Authorization": f"Bearer {api_key}"
-# }
+api_key = os.getenv('api_key')
 
-# response = requests.post('https://api.openai.com/v1/chat/completions', json=data, headers=headers)
+data = {
+    "model": "gpt-3.5-turbo",  
+    "messages": [{"role": "user", "content": "Say this is a test!"}],
+    "temperature": 0.7
+}
 
-# if response.status_code == 200:
-#     print(response.json()['choices'][0]['message']['content'])
-# else:
-#     print(f"Error: {response.status_code}, {response.text}")
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+response = requests.post('https://api.openai.com/v1/chat/completions', json=data, headers=headers)
+
+if response.status_code == 200:
+    print(response.json()['choices'][0]['message']['content'])
+else:
+    print(f"Error: {response.status_code}, {response.text}")
